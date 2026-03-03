ea03e86285219eaaad64980d29eaa9a592d40e1d
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-03 18:02:49 +0200

    Update EventDispatcher.kt

--- src/main/kotlin/com/github/gameringop/event/EventDispatcher.kt
+++ src/main/kotlin/com/github/gameringop/event/EventDispatcher.kt
@@ -39,7 +39,7 @@ object EventDispatcher {
 
     fun init() {
         WorldRenderEvents.END_MAIN.register { context ->
-            EventBus.post(RenderWorldEvent(RenderContext.fromContext(context)))
+            EventBus.post(RenderWorldEvent(RenderContext.fromContext(context), mc.deltaFrameTime.toFloat()))
         }
 
         ClientPlayConnectionEvents.JOIN.register { _, _, _ ->
