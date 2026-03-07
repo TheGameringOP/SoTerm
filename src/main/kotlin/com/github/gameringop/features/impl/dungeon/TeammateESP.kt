9980a1f37ce03fe121ff112d5f5ec467abe1748d
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-07 17:10:19 +0200

    Update TeammateESP.kt

--- src/main/kotlin/com/github/gameringop/features/impl/dungeon/TeammateESP.kt
+++ src/main/kotlin/com/github/gameringop/features/impl/dungeon/TeammateESP.kt
@@ -4,19 +4,28 @@ import com.github.gameringop.event.impl.CheckEntityGlowEvent
 import com.github.gameringop.event.impl.RenderWorldEvent
 import com.github.gameringop.features.Feature
 import com.github.gameringop.ui.clickgui.components.getValue
+import com.github.gameringop.ui.clickgui.components.impl.ColorSetting
 import com.github.gameringop.ui.clickgui.components.impl.ToggleSetting
 import com.github.gameringop.ui.clickgui.components.provideDelegate
 import com.github.gameringop.utils.MathUtils
 import com.github.gameringop.utils.dungeons.DungeonListener
+import com.github.gameringop.utils.dungeons.enums.DungeonClass
 import com.github.gameringop.utils.location.LocationUtils
 import com.github.gameringop.utils.render.Render3D
 import com.github.gameringop.utils.render.RenderHelper.renderVec
 import net.minecraft.client.player.AbstractClientPlayer
 import net.minecraft.world.entity.Entity
+import java.awt.Color
 
 object TeammateESP: Feature("Highlights your dungeon party.") {
     val highlight by ToggleSetting("Highlight Teammates", true)
     val drawName by ToggleSetting("Show Teammate Name", true)
+    
+    private val archerColor by ColorSetting("Archer Color", Color(255, 170, 0), false).section("Class Colors")
+    private val berserkColor by ColorSetting("Berserk Color", Color(170, 0, 0), false)
+    private val mageColor by ColorSetting("Mage Color", Color(85, 255, 255), false)
+    private val healerColor by ColorSetting("Healer Color", Color(255, 85, 255), false)
+    private val tankColor by ColorSetting("Tank Color", Color(0, 170, 0), false)
 
     override fun init() {
         register<CheckEntityGlowEvent> {
@@ -26,7 +35,7 @@ object TeammateESP: Feature("Highlights your dungeon party.") {
 
             for (teammate in DungeonListener.dungeonTeammates) {
                 if (teammate.entity?.id != event.entity.id) continue
-                event.color = teammate.clazz.color
+                event.color = getClassColor(teammate.clazz)
             }
         }
 
@@ -35,13 +44,14 @@ object TeammateESP: Feature("Highlights your dungeon party.") {
             if (! LocationUtils.inDungeon) return@register
             for (teammate in DungeonListener.dungeonTeammatesNoSelf) {
                 val entity = teammate.entity ?: continue
-                val color = teammate.clazz.code
+                val color = getClassColor(teammate.clazz).let { java.awt.Color(it.red, it.green, it.blue) }
+                val hexColor = String.format("#%02x%02x%02x", color.red, color.green, color.blue)
                 val renderVec = entity.renderVec
                 val distance = MathUtils.distance3D(renderVec, mc.player !!.renderVec)
                 val scale = (distance * 0.12f).coerceAtLeast(1.0)
 
                 Render3D.renderString(
-                    "&e[${teammate.clazz.name[0]}&e] $color${teammate.name}",
+                    "&e[${teammate.clazz.name[0]}&e] &$hexColor${teammate.name}",
                     renderVec.x,
                     renderVec.y + entity.bbHeight + 0.7 + distance * 0.015f,
                     renderVec.z,
@@ -49,6 +59,17 @@ object TeammateESP: Feature("Highlights your dungeon party.") {
                     phase = true
                 )
             }
+        }
+    }
+
+    private fun getClassColor(dungeonClass: DungeonClass): Color {
+        return when (dungeonClass) {
+            DungeonClass.Archer -> archerColor.value
+            DungeonClass.Berserk -> berserkColor.value
+            DungeonClass.Mage -> mageColor.value
+            DungeonClass.Healer -> healerColor.value
+            DungeonClass.Tank -> tankColor.value
+            else -> Color.WHITE
         }
     }
 
