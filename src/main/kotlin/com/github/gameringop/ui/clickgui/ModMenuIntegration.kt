855a0d2e198c54169a142fc0e0b3391645ba9a52
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-07 19:17:04 +0200

    Update ModMenuIntegration.kt

--- src/main/kotlin/com/github/gameringop/ui/clickgui/ModMenuIntegration.kt
+++ src/main/kotlin/com/github/gameringop/ui/clickgui/ModMenuIntegration.kt
@@ -2,12 +2,7 @@ package com.github.gameringop.ui.clickgui
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory
 import com.terraformersmc.modmenu.api.ModMenuApi
-import net.minecraft.client.gui.screens.Screen
 
 class ModMenuIntegration : ModMenuApi {
-    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> {
-        return ConfigScreenFactory { parent ->
-            ClickGuiScreen
-        }
-    }
+    override fun getModConfigScreenFactory(): ConfigScreenFactory<*> = ConfigScreenFactory { ClickGuiScreen }
 }
