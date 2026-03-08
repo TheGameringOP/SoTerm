886c2a8a0bd7c7f367f486f1c252a2cf083ab733
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-08 17:57:29 +0200

    Update MixinNameTag.java

--- src/main/java/com/github/gameringop/mixin/MixinNameTag.java
+++ src/main/java/com/github/gameringop/mixin/MixinNameTag.java
@@ -3,21 +3,30 @@ package com.github.gameringop.mixin;
 import com.github.gameringop.features.impl.visual.AlwaysNameTags;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class MixinNameTag {
-    
-    @Shadow
-    public abstract boolean hasCustomName();
-    
+
+    @Inject(method = "shouldShowName", at = @At("HEAD"), cancellable = true)
+    private void onShouldShowName(CallbackInfoReturnable<Boolean> cir) {
+        if (AlwaysNameTags.INSTANCE.getEnabled()) {
+            Entity entity = (Entity) (Object) this;
+            if (entity.hasCustomName()) {
+                cir.setReturnValue(true);
+            }
+        }
+    }
+
     @Inject(method = "isCustomNameVisible", at = @At("HEAD"), cancellable = true)
     private void onIsCustomNameVisible(CallbackInfoReturnable<Boolean> cir) {
-        if (AlwaysNameTags.INSTANCE.enabled && this.hasCustomName()) {
-            cir.setReturnValue(true);
+        if (AlwaysNameTags.INSTANCE.getEnabled()) {
+            Entity entity = (Entity) (Object) this;
+            if (entity.hasCustomName()) {
+                cir.setReturnValue(true);
+            }
         }
     }
 }
