2423eb2ff7fa40b3bb945df483128dcc41fd1443
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-08 17:44:21 +0200

    Create MixinNameTag.java

---
+++ src/main/java/com/github/gameringop/mixin/MixinNameTag.java
@@ -0,0 +1,23 @@
+package com.github.gameringop.mixin;
+
+import com.github.gameringop.features.impl.visual.AlwaysNameTags;
+import net.minecraft.world.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public abstract class MixinNameTag {
+    
+    @Shadow
+    public abstract boolean hasCustomName();
+    
+    @Inject(method = "isCustomNameVisible", at = @At("HEAD"), cancellable = true)
+    private void onIsCustomNameVisible(CallbackInfoReturnable<Boolean> cir) {
+        if (AlwaysNameTags.INSTANCE.enabled && this.hasCustomName()) {
+            cir.setReturnValue(true);
+        }
+    }
+}
