1f04c68225425ccd841602e666e0b8bfbfcb7fe3
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-03-08 18:19:25 +0200

    Delete src/main/kotlin/com/github/gameringop/features/impl/visual/AlwaysNameTags.kt

--- src/main/kotlin/com/github/gameringop/features/impl/visual/AlwaysNameTags.kt
+++
@@ -1,6 +0,0 @@
-// package com.github.gameringop.features.impl.visual
-
-// import com.github.gameringop.features.Feature
-
-// object AlwaysNameTags : Feature("Always shows name tags for named entities.") {
-// }
