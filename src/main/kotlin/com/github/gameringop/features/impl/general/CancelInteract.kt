f770da89cb4797de9816a69ad52f3add9312c7f4
TheGameringOP <46537913+TheGameringOP@users.noreply.github.com>
2026-04-03 14:44:50 +0300

    Update CancelInteract.kt

--- src/main/kotlin/com/github/gameringop/features/impl/general/CancelInteract.kt
+++ src/main/kotlin/com/github/gameringop/features/impl/general/CancelInteract.kt
@@ -1,17 +1,14 @@
 package com.github.gameringop.features.impl.general
 
-import com.github.gameringop.event.impl.PacketEvent
+import com.github.gameringop.event.EventPriority
+import com.github.gameringop.event.impl.PlayerInteractEvent
 import com.github.gameringop.features.Feature
-import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket
 import net.minecraft.world.item.Items
 
 object CancelInteract: Feature("Disables Hypixel's stupid Ender Pearl throw block when you are looking at a wall/floor/ceiling.") {
     override fun init() {
-        register<PacketEvent.Sent> {
-            if (event.packet !is ServerboundUseItemOnPacket) return@register
-            val itemStack = mc.player?.getItemInHand(event.packet.hand) ?: return@register
-            if (itemStack.item != Items.ENDER_PEARL) return@register
-            event.isCanceled = true
+        register<PlayerInteractEvent.RIGHT_CLICK.BLOCK>(EventPriority.LOWEST) {
+            event.item?.takeIf { it.`is`(Items.ENDER_PEARL) }?.let { mc.hitResult = null }
         }
     }
 }
